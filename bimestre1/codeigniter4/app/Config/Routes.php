7f3189b01759ebc050e61544276e32af0f29feec
juan <juanpmad@outlook.com>
2024-03-05 22:14:38 -0300

    bimestre1

--- bimestre1/codeigniter4/app/Config/Routes.php
+++ bimestre1/codeigniter4/app/Config/Routes.php
@@ -10,3 +10,4 @@ $routes->get('/tela2', 'Home::tela2');
 $routes->get('/tela3', 'Home::tela3');
 
 $routes->get('/form', 'Home::form');
+$routes->post('/form_data', 'Home::receive_data');
--- bimestre1/codeigniter4/app/Views/form.php
+++ bimestre1/codeigniter4/app/Views/form.php
@@ -1,37 +1,31 @@
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url('style/style.css');?>">
         <title>Formulário</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     </head>
+
     <body>
-        <form class="row g-3">
-            <div class="col-md-6">
-                <label for="inputArist" class="form-label">Artista</label>
-                <input type="artist" class="form-control" id="inputArtist" placeholder="Lupe de Lupe">
+        <div class="container mt-3">
+        <h2>Formulariozinho</h2>
+        <form action="form_data" method='post'>
+            <div class="mb-3 mt-3">
+            <label for="nome">Nome:</label>
+            <input type="text" class="form-control" id="nome" placeholder="Insira o nome" name="nome">
             </div>
-            <div class="col-md-6">
-                <label for="inputAlbum" class="form-label">Álbum</label>
-                <input type="album" class="form-control" id="inputAlbum" placeholder="Quarup">
+            <div class="mb-3">
+            <label for="date">Data de nascimento:</label>
+            <input type="date" class="form-control" id="date" placeholder="Insira sua data de nascimento" name="date">
             </div>
-            <div class="col-12">
-                <label for="inputAlbumArtist" class="form-label">Artista do Álbum</label>
-                <input type="text" class="form-control" id="inputAlbumArist" placeholder="Lupe de Lupe">
+            <div class="mb-3">
+            <label for="sal">Salário:</label>
+            <input type="money" class="form-control" id="sal" placeholder="Insira o seu salário" name="sal">
             </div>
-            <div class="col-12">
-                <label for="inputTrack" class="form-label">Faixa</label>
-                <input type="text" class="form-control" id="inputTrack" placeholder="Eu Já Venci">
-            </div>
-            <div class="col-12">
-                <button type="submit" class="btn btn-primary">Enviar</button>
-            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
         </form>
+        </div>
     </body>
 </html>
